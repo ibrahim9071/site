d8d2c8e46ff7fa8af6943ea77a928670aeb62459
ibrahim9071 <infinitypo35@gmail.com>
2026-06-06 05:37:29 +0300

    Create app.js

---
+++ iş/app.js
@@ -0,0 +1,27 @@
+const searchInput = document.getElementById("searchInput");
+
+if(searchInput){
+
+searchInput.addEventListener("keyup", function(){
+
+let filter = searchInput.value.toLowerCase();
+
+let cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+
+let title = card.innerText.toLowerCase();
+
+if(title.includes(filter)){
+card.style.display = "block";
+}else{
+card.style.display = "none";
+}
+
+});
+
+});
+
+}
+
+console.log("FLIX TV Hazır");
