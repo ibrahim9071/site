817da342a3dd36f7669fcb1969c1807d1106c2a9
ibrahim9071 <infinitypo35@gmail.com>
2026-06-06 05:56:16 +0300

    Delete iş directory

--- iş/app.js
+++
@@ -1,7 +0,0 @@
-console.log("FLIX TV hazır.");
-
-document.querySelectorAll(".movie-card").forEach(card => {
-    card.addEventListener("click", () => {
-        alert("Film detay sayfası daha sonra eklenecek.");
-    });
-});
